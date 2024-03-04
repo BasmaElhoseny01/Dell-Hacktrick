e461877f5cf80eabe29cf4f5fcc77a01bbf7ab99
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-03-04 18:00:15 +0200

    Added URL

--- Solvers/eagle_submission_solver.py
+++ Solvers/eagle_submission_solver.py
@@ -6,9 +6,9 @@ from get_eagle import get_eagle_model
 
 api_base_url = None
 #TODO: Set the api_base_url to the base url of the API when Ready
-# api_base_url = "http://3.70.97.142:5000/"
-# team_id=None
+api_base_url = "http://3.70.97.142:5000/"
 team_id="hAaIrJk"
+# team_id=None
 DEBUG = True
 
 def init_eagle(team_id):
