38c301f68d13116c29de817de354e1d3b9bc3bc9
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-03-05 20:30:56 +0200

    Added Split Full Data

--- eagle/data.py
+++ eagle/data.py
@@ -17,6 +17,40 @@ def preprocessing(data_spectrogram):
   # gaussian_avg_spectrogram = gaussian_filter(data_spectrogram, sigma=1)
   return data_spectrogram
 
+
+
+def Split_Data_full_data(real_dataset_path:str,fake_dataset_path:str,train_val_test_ratio:tuple=(0,0,0)):
+  #TODO fix ratio of split
+  # Load the spectrogram data from the .npz file
+  real_data = np.load(real_dataset_path)
+  real_data_spectrogram =real_data['x'][:,:real_data['x'].shape[1]-1,:]
+  real_data_labels =real_data['y']
+  # x=spectrogram_data_real['x'] #(750, 1998, 101)
+  # y=spectrogram_data_real['y'] #(750, 496)
+
+  # Load the spectrogram data from the .npz file
+  fake_data = np.load(fake_dataset_path)
+  # remove last row in fake data
+  fake_data_spectrogram =fake_data['x'][:,:fake_data['x'].shape[1]-1,:]
+  fake_data_labels =fake_data['y']
+
+
+  # X_train, X_test, Y_train, Y_test = train_test_split(spectrogram_data, labels, test_size=0.2, random_state=42)
+  # X_train_real ,X_test_real, Y_train_real, Y_test_real= train_test_split(real_data_spectrogram, real_data_labels, test_size=0.2, random_state=40)
+  # X_train_fake ,X_test_fake, Y_train_fake, Y_test_fake= train_test_split(fake_data_spectrogram, fake_data_labels, test_size=0.2, random_state=50)
+  # Conatnate real and fake data
+  X_train=np.concatenate((real_data_spectrogram,fake_data_spectrogram),axis=0)
+  Y_train=np.concatenate((real_data_labels,fake_data_labels),axis=0)
+
+  # shuffle the data
+  indices = np.random.permutation(len(Y_train))
+  X_train = X_train[indices]
+  Y_train = Y_train[indices]
+
+
+  return X_train,Y_train
+
+
 def Split_Data(real_dataset_path:str,fake_dataset_path:str,train_val_test_ratio:tuple=(0,0,0)):
   #TODO fix ratio of split
   # Load the spectrogram data from the .npz file
